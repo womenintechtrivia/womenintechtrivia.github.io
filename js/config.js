5244ec8e7bb1d69ce0dc6c7b0828f3dfd45d911d
Khushboo Peswani <peswani.khushboo19@gmail.com>
2018-10-23 09:44:11 +0530

    WiT Tech trivia

---
+++ js/config.js
@@ -0,0 +1,28 @@
+quizData = [
+	{
+		"question":"Lean in: Women, work and the will to lead” is a book written by?",
+		"questionOpt1":"Sandra Lerner",
+		"questionOpt2":"Sheryl Sandberg",
+		"questionOpt3":"Marrisa Mayor",
+		"questionOpt4":"Meg Whitman",
+		"rightAnswer":"questionOpt2",
+	},
+	{
+		"question":"She was an analyst of Charles Babbage’s analytical engine and is considered by many the “first computer programmer. Who’s she?",
+		"questionOpt1":"Ada Lovelace",
+		"questionOpt2":"Grace Hopper",
+		"questionOpt3":"Ursula Burns",
+		"questionOpt4":"Sophie Wilson",
+		"rightAnswer":"questionOpt1",
+	},
+	{
+		"question":"Which women developed the algorithm behind the spanning tree protocol?",
+		"questionOpt1":"Radia Perlman",
+		"questionOpt2":"Tarah wheeler",
+		"questionOpt3":"Marrisa Mayor",
+		"questionOpt4":"Jean E. Sammet",
+		"rightAnswer":"questionOpt1",
+	},
+
+]
+
